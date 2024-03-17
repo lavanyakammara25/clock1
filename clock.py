11eeb03bd0ee21f5be0ddeae2bec732a794a722a
lavanyakammara25 <161356945+lavanyakammara25@users.noreply.github.com>
2024-03-17 14:26:40 +0530

    Add files via upload

---
+++ clock.py
@@ -0,0 +1,20 @@
+from tkinter import *
+from time import *
+
+def update():
+    time_string = strftime("%I:%M:%S %p")
+    time_label.config(text=time_string)
+
+    
+    window.after(1000,update)
+
+window = Tk()
+
+time_label = Label(window,font=("Arial",50),fg="#00FF00",bg="black")
+time_label.pack()
+
+
+
+update()
+
+window.mainloop()
